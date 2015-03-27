0ec85a8d94f1f2cbf7a9a0bed89ff43de5b66440
Samuel Olivier <skanight@hotmail.fr>
2015-03-27 23:13:47 +0100

    Changed Camera Control, added stats, refactoring architecture (file and code)

--- projects/ClothSimulation/js/clothSimulation.js
+++ projects/ClothSimulation/js/clothSimulation.js
@@ -1,59 +1,43 @@
-var clock = new THREE.Clock();
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.shadowMapEnabled = true;
-document.body.appendChild( renderer.domElement );
-
-var trackballControls = new THREE.TrackballControls(camera);
-trackballControls.rotateSpeed = 1.0;
-trackballControls.zoomSpeed = 1.0;
-trackballControls.panSpeed = 1.0;
-//        trackballControls.noZoom=false;
-trackballControls.noPan=false;
-trackballControls.staticMoving = true;
-//        trackballControls.dynamicDampingFactor=0.3;
-
-var cloth = new Cloth(1.6, 0.9, 20, 20, 0.3);
-cloth.mesh.castShadow = true;
-scene.add( cloth.mesh );
-
-var planeGeometry = new THREE.PlaneGeometry( 10, 10, 1);
-var planeMaterial = new THREE.MeshLambertMaterial( { color: 0x848484, side: THREE.DoubleSide } );
-var plane = new THREE.Mesh( planeGeometry, planeMaterial );
-plane.position.y = -2;
-plane.rotation.x = Math.PI / 2;
-plane.receiveShadow = true;
-scene.add( plane );
-
-camera.position.z = 3;
-
-var light = new THREE.SpotLight( 0xC0BFAD, 1.5 );
-light.position.set( 5, 20, 10 );
-light.castShadow = true;
-light.shadowMapWidth = 1024;
-light.shadowMapHeight = 1024;
-
-light.shadowCameraNear = 5;
-light.shadowCameraFar = 100;
-light.shadowCameraFov = 30;
-light.shadowCameraVisible = true;
-scene.add( light );
-
-var ambientLight = new THREE.AmbientLight(0x383838);
-scene.add(ambientLight);
-
-
 var render = function () {
 	var delta = clock.getDelta();
 
-    trackballControls.update(delta);
-    cloth.update(delta);
-
+    orbitControls.update(delta);
+    scene.cloth.update(delta);
+    stats.update(delta);
 	requestAnimationFrame( render );
-	renderer.render(scene, camera);
+	renderer.render(scene.scene, scene.camera);
 };
 
+function onWindowResize() {
+	scene.camera.aspect = window.innerWidth / window.innerHeight;
+	scene.camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);				
+	render();
+}
+
+function init() {
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.shadowMapEnabled = true;
+	document.body.appendChild( renderer.domElement );
+
+	scene = new Scene();
+
+	orbitControls = new THREE.OrbitControls(scene.camera, renderer.domElement);
+	orbitControls.maxDistance = 50;
+
+	clock = new THREE.Clock();
+
+	stats = new Stats();
+	stats.domElement.style.position = 'absolute';
+	stats.domElement.style.top = '0px';
+	stats.domElement.style.zIndex = 100;
+	document.body.appendChild(stats.domElement);
+
+	window.addEventListener('resize', onWindowResize, false);
+}
+
+var stats, renderer, scene, orbitControls, clock;
+
+init();
 render();
---
+++ projects/ClothSimulation/js/scene.js
@@ -0,0 +1,34 @@
+function Scene() {
+	this.scene = new THREE.Scene();
+	this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+	this.camera.position.z = 3;
+
+	this.cloth = new Cloth(1.6, 0.9, 20, 20, 0.3);
+	this.cloth.mesh.castShadow = true;
+	this.scene.add( this.cloth.mesh );
+
+	var planeGeometry = new THREE.PlaneGeometry( 10, 10, 1);
+	var planeMaterial = new THREE.MeshLambertMaterial( { color: 0x848484, side: THREE.DoubleSide } );
+	this.plane = new THREE.Mesh( planeGeometry, planeMaterial );
+	this.plane.position.y = -2;
+	this.plane.rotation.x = Math.PI / 2;
+	this.plane.receiveShadow = true;
+	this.scene.add( this.plane );
+
+
+	this.light = new THREE.SpotLight( 0xC0BFAD, 1.5 );
+	this.light.position.set( 5, 20, 10 );
+	this.light.castShadow = true;
+	this.light.shadowMapWidth = 1024;
+	this.light.shadowMapHeight = 1024;
+
+	this.light.shadowCameraNear = 5;
+	this.light.shadowCameraFar = 100;
+	this.light.shadowCameraFov = 30;
+	this.light.shadowCameraVisible = true;
+	this.scene.add( this.light );
+
+	this.ambientLight = new THREE.AmbientLight(0x383838);
+	this.scene.add(this.ambientLight);
+}
+
